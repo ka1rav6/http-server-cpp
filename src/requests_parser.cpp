6b143f9a10329864255fe23b3df5a8d8ebcdd04d
Kairav <kairavdutta@gmail.com>
2026-05-11 19:44:00 +0530

    Improved requests parsing

--- src/requests_parser.cpp
+++ src/requests_parser.cpp
@@ -1,14 +1,17 @@
 #include "../include/requests_parser.h"
 #include <sstream>
 
-
 Message* parse(const std::string& msg){
+
     Message* final = new Message;
     std::stringstream ss(msg);
     ss >> final->type;
     ss >> final->file;
     std::string line;
+    std::getline(ss, line);
     while (std::getline(ss, line)){
+        if (!line.empty() && line.back() == '\r')
+            line.pop_back();
         if (line.find("Host: ") == 0){
             final->host = line.substr(6);
         }
